13c71f96ed1dfeff820dac7a7981ca04d9c78220
Kevin Garibo <garibokevin@gmail.com>
2016-01-03 12:17:54 -0600

    Aplicacion empezada

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,54 @@
+class CommentsController < ApplicationController
+  before_action :set_comment, only: [:update, :destroy]
+  before_action :set_article
+  before_action :authenticate_user!
+
+  respond_to :html
+
+  def create
+    @comment = current_user.comments.new(comment_params)
+    @comment.article = @article
+    respond_to do |format|
+      if @comment.save
+        format.html {redirect_to @comment.article, notice: "El comentario se anadio exitosamente"}
+        format.json {render :show, status: :created, location: @comment}
+      else
+        format.html {render :new}
+        format.json {render json: @comment.errors, status: :unprocessable_entity}
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html {redirect_to @comment.article, notice: "El comentario se añadio exitosamente"}
+        format.json {render :show, status: :ok, location: @comment}
+      else
+        format.html {render :edit}
+        format.json {render json: @comment.errors, status: :unprocessable_entity}
+      end
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    respond_to do |format|
+      format.html {redirect_to comments_url, notice: "El comentario se ha eliminado"}
+      format.json {head :no_content}
+    end
+  end
+
+  private
+    def set_article
+      @article = Article.find(params[:article_id])
+    end
+
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
+    def comment_params
+      params.require(:comment).permit(:user_id, :article_id, :body)
+    end
+end
