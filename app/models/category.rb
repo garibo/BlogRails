12090862e3b0274175cb3e231171b04b61124c12
Kevin Garibo <garibokevin@gmail.com>
2016-01-03 20:15:45 -0600

    Categorias agregadas

---
+++ app/models/category.rb
@@ -0,0 +1,5 @@
+class Category < ActiveRecord::Base
+	validate :name, presence: true
+	has_many :has_categories
+	has_many :articles, through: :has_categories
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160103195148) do
+ActiveRecord::Schema.define(version: 20160103233330) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 20160103195148) do
 
   add_index "articles", ["user_id"], name: "index_articles_on_user_id"
 
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.string   "color"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "comments", force: true do |t|
     t.integer  "user_id"
     t.integer  "article_id"
@@ -38,6 +45,16 @@ ActiveRecord::Schema.define(version: 20160103195148) do
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id"
   add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+
+  create_table "has_categories", force: true do |t|
+    t.integer  "article_id"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "has_categories", ["article_id"], name: "index_has_categories_on_article_id"
+  add_index "has_categories", ["category_id"], name: "index_has_categories_on_category_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
